60236a928b76f1c70090fcf9b54c8d25a62de82a
Paige-PC\Paige Tyler <paige.tyler@myokanagan.bc.ca>
2017-03-08 13:32:13 -0800

    add php to create account page

--- createAccount.php
+++ createAccount.php
@@ -7,7 +7,7 @@
 
 <body>
 	<h1>Create Account</h1>
-	<form>
+	<form action="" method="POST">
 		Username:<br>
 		<input type="text" name="username"><br>
 		First name:<br>
@@ -28,4 +28,32 @@
 		<input type="reset" ></input>
    </form>
 </body>
+
+<?php
+echo $_POST['username'];
+echo $_POST['fname'];
+echo $_POST['lname'];
+echo $_POST['authority'];
+echo $_POST['password'];
+
+$username="teamvwt@gmail.com";
+$password="_2\"N%'sR8[p6";
+$database="information_schema";
+
+$con=mysqli_connect("localhost",$username,$password,$database);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+  mysqli_query($con,"INSERT INTO employees (username, fName, lName, auth, pass) 
+VALUES (username, fname, lname, authority, password)");
+
+// Print auto-generated id
+echo "New record has id: " . mysqli_insert_id($con);
+
+mysqli_close($con); 
+//put header here to redirect back to login page
+?>
 </html>
